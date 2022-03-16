b466a27aee9c7f61910973ec576b1d74ba9ceb1f
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-16 23:50:29 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <Top />
+    <div class="w-10/12 mx-auto">
+      <h1>hello</h1>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Header: () => import('~/components/Header'),
+    Top: () => import('~/components/Top'),
+    Footer: () => import('~/components/Footer'),
+  },
+}
+</script>
