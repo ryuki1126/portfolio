983205c5bc82be948da24a40f3de5130ad119058
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-18 10:39:43 +0900

    fixed

---
+++ components/Skills.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-10/12 pt-4 pb-8 mx-auto">
+    <h3 class="text-center text-white text-3xl font-bold">SKILLS</h3>
+    <div class="w-20 h-1 mt-2 mx-auto bg-green-500"></div>
+    <div class="pt-12 flex justify-center">
+      <div>
+        <img class="mask mask-hexagon-2" src="https://api.lorem.space/image/shoes?w=160&h=160" />
+        <ul class="pt-4 text-white text-lg font-bold">
+          <li>名前 : 山本竜輝</li>
+          <li>誕生日 : 1996/11/26</li>
+          <li>居住地 : 大阪</li>
+          <li>職業 : webエンジニア</li>
+          <li>趣味 : ドライブ、ポケモン</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
